5a8adf379f741575e8f1918ac32a3f793e026169
TaeWooKim-SCH <zop1234@hanmail.net>
2023-09-15 15:26:15 +0900

    settings: 폴더 구조 및 폰트 셋팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-red-500">안녕하세요.</h1>
+      <h1 className="text-red-500 text-[3rem] font-[800]">안녕하세요.</h1>
+      <h1 className="text-red-500 text-[3rem] font-[800]">Hello world.</h1>
     </main>
   )
 }
