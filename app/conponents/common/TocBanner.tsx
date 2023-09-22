438a93abd8fde4978a6e4648c6c354d2f1746dcd
TaeWooKim-SCH <zop1234@hanmail.net>
2023-09-22 22:27:15 +0900

    feat: 상세 페이지 디자인 수정

--- app/conponents/common/TocBanner.tsx
+++ app/conponents/common/TocBanner.tsx
@@ -15,31 +15,31 @@ export default function TocBanner({ headings }: PropsType) {
   }
 
   return (
-    <div className="overflow-hidden rounded-xl border border-neutral-200 transition-all">
-      <div className="p-4 pr-2 dark:border-neutral-700">
+    <div className="overflow-hidden rounded-xl border border-neutral-200 transition-all dark:bg-neutral-800 dark:border-none">
+      <div className="p-4 pr-2">
         <p className="font-bold dark:text-[#dddddd]">On this page</p>
         <ul className="mt-2 text-sm">
           {headings.map((head, idx) => (
             head.level === 1 || head.level === 2 ? (
               <li className="py-1" key={idx}>
-                <a className="dark:text-[#dddddd]" href={`#${head.slug}`}>{head.text}</a>
+                <a className="dark:text-[#a1a1a1] dark:hover:text-[#dddddd]" href={`#${head.slug}`}>{head.text}</a>
               </li>
             ) : (
               <li className="py-1 ml-2" key={idx}>
-                <a className="flex items-center dark:text-[#dddddd] text-xs" href={`#${head.slug}`}>
-                  <HiChevronRight className="dark:fill-[#dddddd]" /> {head.text}
+                <a className="flex items-center dark:text-[#a1a1a1] dark:hover:text-[#dddddd] text-xs" href={`#${head.slug}`}>
+                  <HiChevronRight className="dark:fill-[#a1a1a1]" /> {head.text}
                 </a>
               </li>
             )
           ))}
         </ul>
       </div>
-      <div className="flex items-center justify-between p-4 bg-neutral-200">
+      <div className="flex items-center justify-between p-4 bg-neutral-200 dark:bg-neutral-700">
         <button onClick={copyLinkHandler}>
-          <BiLink size="25" />
+          <BiLink className="dark:fill-[#a1a1a1]" size="25" />
         </button>
         <button onClick={topScrollHandler}>
-          <HiMiniArrowUturnUp size="20" />
+          <HiMiniArrowUturnUp className="dark:fill-[#a1a1a1]" size="20" />
         </button>
       </div>
     </div>
--- app/conponents/layouts/PostLayout.tsx
+++ app/conponents/layouts/PostLayout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { AiOutlineCalendar } from 'react-icons/ai';
 
 import Layout from "./Layout";
@@ -5,6 +6,8 @@ import Title from "../common/Title";
 import Hr from '../common/Hr';
 import Mdx from '../common/Mdx';
 import TocBanner from '../common/TocBanner';
+import Tag from '../common/Tag';
+import AuthorContacts from '../common/AuthorContacts';
 
 export default function PostLayout({ post }: any) {
   return (
@@ -19,9 +22,25 @@ export default function PostLayout({ post }: any) {
       <Hr className="mb-14" />
       <div className="flex justify-center relative gap-8 w-full">
         <Mdx data={post.body.code} />
-        <div className="ml-10">
+        <div className="ml-auto">
           <div className="sticky top-[50px] min-w-[240px] max-w-[260px]">
             <TocBanner headings={post.headings} />
+          </div>
+        </div>
+      </div>
+      <div className="mt-24">
+        <div className="flex flex-wrap items-center gap-2">
+          {post.tags.map((tag: string, i: number) => (
+            <Tag key={i}>{tag}</Tag>
+          ))}
+        </div>
+        <Hr className="my-10" />
+        <div className="flex justify-center items-center">
+          <Image className="w-24 h-24 rounded-full mr-8" src="/blogImgs/profile.jpg" alt="프로필 사진" width={100} height={100} />
+          <div>
+            <div className="font-bold dark:text-[#dddddd]">TaeWoo Kim</div>
+            <div className="text-[#a1a1a1] mb-2">Junior Frontend Engineer</div>
+            <AuthorContacts />
           </div>
         </div>
       </div>
