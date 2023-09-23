1fa6e3a5cf07df4f13d696bee618e20d4d9f2182
TaeWooKim-SCH <zop1234@hanmail.net>
2023-09-23 17:01:29 +0900

    fix: 이전 코드로 복원

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,67 @@
 import Layout from "../conponents/layouts/Layout";
 import Title from "../conponents/common/Title";
-import BlogMain from "../conponents/BlogMain";
+import { AllBlogPost, filteredSeriesBook } from "../libs/dataset";
+import SubTitle from "../conponents/common/SubTitle";
+import PostListItem from "../conponents/common/PostListItem";
 
 export default async function Blog() {
   return (
     <Layout>
       <Title>Blog</Title>
-      <BlogMain />
+      <div>
+        <div className="text-[#555555] dark:text-[#dddddd]">개발하며 알게된 것들을 기록하는 공간입니다.</div>
+
+        <div className="flex items-center space-x-6 py-12 overflow-scroll scrollbar-hide">
+          {filteredSeriesBook.map((post, idx) => (
+            <div key={idx}>
+              <a href={post.slug}>
+                <div className="relative h-56 w-40 select-none rounded-lg bg-neutral-200 px-8 pt-8 pb-12 shadow-lg transition-all hover:scale-[1.01] hover:shadow-xl dark:bg-neutral-800">
+                  <div className="absolute inset-y-0 left-2.5 w-[1px] bg-neutral-100 dark:bg-neutral-700"></div>
+                  <div className="flex h-full break-keep bg-white px-2 py-3 text-sm font-medium dark:bg-neutral-700 dark:text-[#dddddd]">{post.title}</div>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      
+        <div className="mt-16 flex">
+          <SubTitle>{'All Posts'}</SubTitle>
+          <span className="pt-3 ml-2 text-lg font-bold dark:text-[#dddddd]">{'('}{AllBlogPost.length}{')'}</span>
+        </div>
+
+        <div className="grid gap-8 grid-cols-1 md:grid-cols-2 mt-12">
+          {AllBlogPost.map((post, idx) => (
+            <PostListItem post={post} key={idx} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
+
+// export const defaultEasing = [0.6, -0.05, 0.01, 0.99];
+
+// export const fadeInSlideToLeft = {
+//   initial: {
+//     opacity: 0,
+//     x: 30,
+//     transition: { duration: 0.6, ease: defaultEasing },
+//     willChange: 'opacity, transform',
+//   },
+//   animate: {
+//     opacity: 1,
+//     x: 0,
+//     transition: { duration: 0.6, ease: defaultEasing },
+//     willChange: 'opacity, transform',
+//   },
+//   exit: {
+//     opacity: 0,
+//     x: -30,
+//     transition: { duration: 0.6, ease: defaultEasing },
+//     willChange: 'opacity, transform',
+//   },
+// };
+
+// export const staggerOne: Variants = {
+//   animate: { transition: { staggerChildren: 0.1 } },
+// };
