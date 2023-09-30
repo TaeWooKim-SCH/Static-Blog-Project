0a5204b752197fc2bd0507e41ebf913f6e0fe603
TaeWooKim-SCH <zop1234@hanmail.net>
2023-09-30 17:09:19 +0900

    fix: 블로그 페이지 디자인 수정

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,6 @@ import Title from "../conponents/common/Title";
 import { AllBlogPost, filteredSeriesBook } from "../libs/dataset";
 import SubTitle from "../conponents/common/SubTitle";
 import PostListItem from "../conponents/common/PostListItem";
-import Hr from "../conponents/common/Hr";
 
 export default async function Blog() {
   return (
@@ -31,7 +30,7 @@ export default async function Blog() {
           <span className="pt-[0.4rem] lg:pt-3 ml-2 lg:text-lg font-bold dark:text-[#dddddd]">{'('}{AllBlogPost.length}{')'}</span>
         </div>
 
-        <div className="grid gap-8 grid-cols-1 mt-12">
+        <div className="grid gap-8 grid-cols-1 mt-12 md:grid-cols-2">
           {AllBlogPost.map((post, idx) => (
             <PostListItem post={post} key={idx} />
           ))}
--- app/conponents/common/PostListItem.tsx
+++ app/conponents/common/PostListItem.tsx
@@ -7,10 +7,10 @@ export default function PostListItem({ post }: any) {
   const href = `${post.slug}`;
 
   return (
-    <div className="text-ye group w-full px-6 py-4 transition-all border-b-2 border-neutral-300 group">
+    <div className="text-ye group w-full px-6 py-4 transition-all hover:bg-neutral-200 dark:hover:bg-neutral-700 rounded-xl">
       <Link href={href}>
-        <p className="transition-all text-xl font-bold dark:text-[#dddddd] group-hover:drop-shadow-[0_0_10px_#ffee02c5]">{post.title}</p>
-        <p className="transition-all text-[#7a7a7a] mt-1 group-hover:drop-shadow-[0_0_10px_#ffee02c5]">{post.description}</p>
+        <p className="transition-all text-xl font-bold dark:text-[#dddddd]">{post.title}</p>
+        <p className="transition-all text-[#7a7a7a] mt-1">{post.description}</p>
         <div className="mt-2 inline-flex w-full items-start gap-2 text-sm">
           <div className="flex flex-wrap items-center gap-2">
             {post.tags.map((tag: string, i: number) => (
